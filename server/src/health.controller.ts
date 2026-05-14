d31f13c0a95e98290eb997d083663f157a64958e
Mortada-Houmani <mortadahoumani2006@gmail.com>
2026-05-14 20:50:20 +0300

    push test for kereo2

--- server/src/health.controller.ts
+++ server/src/health.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-// push test for my project kereo
+// push test for my project kereo2
 @Controller('health')
 export class HealthController {
   @Get()
